e882015fef10ac5ff350b6146b48bcd570453cf5
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-06 18:32:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,12 +33,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  counting_hash = hash
-  counting_hash[key]
-  if counting_hash[key]
-    counting_hash[key] += 1 
-  end
-  return hash
+ counting_hash = hash 
+ counting_hash[key]
   
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
